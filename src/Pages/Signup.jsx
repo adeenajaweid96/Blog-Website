a375c84fae755114d06cb1ca2cd3adf6601dd2d8
Adeena Jaweid <adeenajaweid96@gmail.com>
2025-10-20 23:48:04 +0500

    .

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Signup = () => {
   return (
     <div className='max-w-screen-2xl mx-auto'>
-      <h1>sign up form</h1>
+      <h1 className=''>sign up form</h1>
 
     </div>
   )
